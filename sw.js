1c66fa0ec9611ce51c0639564a2578f3f44346ea
jrsy2027a <jrsy2027@gmail.com>
2026-07-13 20:23:00 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Misbah-ul-Quran — service worker
    Bump CACHE version whenever you update the app so users get the new build. */
-const CACHE = 'misbah-v9';
+const CACHE = 'misbah-v12';
 
 const SHELL = [
   './',
